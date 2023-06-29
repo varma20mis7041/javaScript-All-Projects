49cb4d52d41ff6206b5f2c128b9ef6fc24c38cea
Bhargav varma <80447853+varma20mis7041@users.noreply.github.com>
2023-06-29 17:13:33 +0530

    Update index.js

--- 1-traffic lights/index.js
+++ 1-traffic lights/index.js
@@ -1,1 +1,39 @@
+let stopButtonb = document.getElementById("stopButton");
+let readyButtonb = document.getElementById("readyButton");
+let goButtonb = document.getElementById("goButton");
+let stopLight = document.getElementById("stopLight");
+let readyLight = document.getElementById("readyLight");
+let goLight = document.getElementById("goLight");
 
+function stopButton() {
+    stopButtonb.style.backgroundColor = "#cf1124";
+    stopLight.style.backgroundColor = "#cf1124";
+    // turning of remaining colors
+    readyButtonb.style.backgroundColor = "#1f1d41";
+    readyLight.style.backgroundColor = "#4b5069";
+    goButtonb.style.backgroundColor = "#1f1d41";
+    goLight.style.backgroundColor = "#4b5069";
+}
+
+function readyButton() {
+    readyButtonb.style.backgroundColor = "#f7c948";
+    readyLight.style.backgroundColor = "#f7c948";
+    // turning of remaining colors
+    stopButtonb.style.backgroundColor = "#1f1d41";
+    stopLight.style.backgroundColor = "#4b5069";
+    goButtonb.style.backgroundColor = "#1f1d41";
+    goLight.style.backgroundColor = "#4b5069";
+
+}
+
+function goButton() {
+    goButtonb.style.backgroundColor = "#199473";
+    goLight.style.backgroundColor = "#199473";
+
+    // turning of remainingcolors
+    readyButtonb.style.backgroundColor = "#1f1d41";
+    readyLight.style.backgroundColor = "#4b5069";
+
+    stopButtonb.style.backgroundColor = "#1f1d41";
+    stopLight.style.backgroundColor = "#4b5069";
+}
